4ca495ae233882aabeecf32b6f7029b985ef22d9
hussam-supy-io <hussam@supy.io>
2023-08-09 11:45:16 +0400

    chore: update package version in user agent

--- lib/cerbos_http_client.dart
+++ lib/cerbos_http_client.dart
@@ -10,7 +10,7 @@ import 'src/externals.dart';
 
 export 'src/externals.dart';
 
-const _defaultUserAgent = 'cerbos-sdk-dart-http/1.0.0';
+const _defaultUserAgent = 'cerbos-sdk-dart-http/0.0.1';
 
 const _checkResources = '/check/resources';
 
