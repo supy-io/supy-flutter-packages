f49ac762334a9d03d55f3d6bead7073167530d19
hussam-supy-io <hussam@supy.io>
2023-08-10 16:39:30 +0400

    fix: attributes jsonKey in Resource.fromMap

--- packages/cerbos_http_client/lib/cerbos_http_client.dart
+++ packages/cerbos_http_client/lib/cerbos_http_client.dart
@@ -9,7 +9,7 @@ import 'src/externals.dart';
 
 export 'src/externals.dart';
 
-const _defaultUserAgent = 'cerbos-sdk-dart-http/0.0.1';
+const _defaultUserAgent = 'cerbos-sdk-dart-http/0.0.2';
 
 const _checkResources = '/check/resources';
 
