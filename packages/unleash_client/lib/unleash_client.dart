a5152209c7a3e0cc061ac4bf91967bdeee942835
abed-supy-io <abed@supy.io>
2025-01-13 23:25:13 +0400

    fix: Update Packages && Dependencies

--- packages/unleash_client/lib/unleash_client.dart
+++ packages/unleash_client/lib/unleash_client.dart
@@ -1,10 +1,10 @@
 /// A dart client for Unleash. Unleash is a simple and easy to use feature
 /// toggle (also known as feature flags) service.
-library unleash_client;
+library;
 
-export 'src/strategy.dart';
-export 'src/unleash.dart';
 export 'src/context.dart';
-export 'src/unleash_settings.dart';
 export 'src/features.dart';
+export 'src/strategy.dart';
 export 'src/toggle_backup/toggle_backup.dart';
+export 'src/unleash.dart';
+export 'src/unleash_settings.dart';
